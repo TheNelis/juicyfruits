8f17cc1f6ba17ed0f6b1d6013479a8d261434058
Niels Hos <118888675+TheNelis@users.noreply.github.com>
2026-02-09 20:12:51 +0100

    changed layout + basically everything

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -29,7 +27,7 @@ const main = document.getElementById('container');
 if (window.innerWidth >= 1024) {
     main.style.height = '30000px';
 } else {
-    main.style.height = '20000px';
+    main.style.height = '15000px';
 }
 
 const scene = new THREE.Scene();
@@ -66,35 +64,46 @@ scene.add(pointLight, ambientLight);
 
 
 
+// Init fontsize
+function fitTextToWidth(el) {
+    let size = 16;
+    let padding = window.innerWidth > 1024 ? 60 : 20;
+    el.style.fontSize = size + 'px';
+
+    while (el.offsetWidth < window.innerWidth - padding) {
+        size++;
+        el.style.fontSize = size + 'px';
+        console.log('window:' + window.innerWidth + ', text:' + el.offsetWidth);
+    }
+}
+
+const name = document.getElementById('name');
+fitTextToWidth(name);
+
+
+
 // 3D models
 const loader = new GLTFLoader();
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/');
 loader.setDRACOLoader(dracoLoader);
 
-const objectsArray = [
-    '/banana.glb',
-    '/apple.glb',
-    '/orange.glb',
-    '/pear.glb',
-    '/strawberry.glb',
+const fruits = [
+    { name: "bananas", color: "#fac919", path: "/banana.glb" },
+    { name: "strawberries", color: "#FF6347", path: "/strawberry.glb" },
+    { name: "pears", color: "#afc744", path: "/pear.glb" },
+    { name: "oranges", color: "#FFA500", path: "/orange.glb" },
+    { name: "apples", color: "#f5795d", path: "/apple.glb" },
 ];
-const fruitColors = [
-    "#fac919", // banana
-    "#f5795d", // apple
-    "#FFA500", // orange
-    "#afc744", // pear
-    "#FF6347", // strawberry
-];
+  
+
 const group = new THREE.Group();
 
 scene.add(group);
 
-const radius = 70;
-
-const promises = objectsArray.map((path, index) => {
+const promises = fruits.map((fruit, index) => {
     return new Promise((resolve) => {
-        loader.load(path, (gltf) => {
+        loader.load(fruit.path, (gltf) => {
             const obj = gltf.scene;
             
             if (window.innerWidth >= 1024) {
@@ -103,32 +112,28 @@ const promises = objectsArray.map((path, index) => {
                 obj.scale.set(20, 20, 20);
             }
 
-            resolve({ obj, index });
+            resolve({ obj, fruit, index });
         });
     });
 });
 
-// GSAP animation
+
 Promise.all(promises).then((results) => {
 
-    results.sort((a, b) => a.index - b.index);
+    const order = [1, 2, 3, 4];
+    const angleStep = (Math.PI * 2) / fruits.length;
+    const radius = 70;
 
-    results.forEach((res) => {
-        const index = res.index;
-        const obj = res.obj;
+    const first = results.find(r => r.index === 0);
+    first.obj.position.set(0, 0, radius);
+    group.add(first.obj);
 
-        let x, z;
-        if (index === 0) {
-            x = 0;
-            z = radius;
-        } else {
-            const angleStep = (Math.PI * 2) / objectsArray.length;
-            const angle = index * angleStep;
-
-            x = Math.sin(angle) * radius;
-            z = Math.cos(angle) * radius;
-        }
-
+    order.forEach((fruitIndex, i) => {
+        const res = results.find(r => r.index === fruitIndex);
+        const { obj } = res;
+        const angle = -(i + 1) * angleStep;
+        const x = Math.sin(angle) * radius;
+        const z = Math.cos(angle) * radius;
         obj.position.set(x, 0, z);
         group.add(obj);
     });
@@ -143,15 +148,26 @@ Promise.all(promises).then((results) => {
         }
     });
 
-    document.getElementById('container').style.backgroundColor = fruitColors[0];
+    document.getElementById('container').style.backgroundColor = fruits[0].color;
     
     for (let i = 0; i < group.children.length; i++) {
-        let step = (Math.PI * -2) / group.children.length * (i + 1);
-        tl.to(group.children[i].rotation, { x: Math.PI * -4, duration: 2, ease: "power1.out" });
+        let step = (Math.PI * 2) / group.children.length * (i + 1);
         
         if (i != group.children.length - 1) {
             tl.to(group.rotation, { y: step, duration: 1, ease: "power2.inOut" });
-            tl.to('#container', { backgroundColor: fruitColors[i + 1], duration: 1, ease: "power2.inOut" }, "<");
+            tl.to('#container', { backgroundColor: fruits[i + 1].color, duration: 1, ease: "power2.inOut" }, "<");
+            tl.to('#namespan', { y: '150%', duration: 0.5, ease: "power2.inOut" }, "<");
+            tl.set('#namespan', { 
+                textContent: fruits[i + 1].name,
+                onComplete: () => {
+                    fitTextToWidth(name);
+                },
+                onReverseComplete: () => {
+                    fitTextToWidth(name);
+                }
+            }, "-=0.5");
+            tl.set('#scrolltext', { textContent: `Scroll for more fruits (${i + 2}/5)`}, "-=0.5");
+            tl.to('#namespan', { y: '0%', duration: 0.5, ease: "power2.inOut" }, "-=0.5");
         }
     }
 });
@@ -162,7 +178,8 @@ Promise.all(promises).then((results) => {
 function animate() {
     requestAnimationFrame(animate);
     group.children.forEach((obj) => {
-        obj.rotation.y += 0.01
+        obj.rotation.y -= 0.02
+        obj.rotation.x -= 0.01
     });
     // controls.update();
     renderer.render(scene, camera);
